c058b08fe5699feb50278cdc3130895355ddbba5
Srilekha Kirsh <skirsh@mozilla.com>
2022-02-17 09:19:37 -0800

    nit

--- .aws/src/backfillLambda.ts
+++ .aws/src/backfillLambda.ts
@@ -21,54 +21,50 @@ export class BackfillLambda extends Resource {
 
     //note: because this is one time migration,
     //we will just use the default event bus.
-    new PocketEventBridgeWithLambdaTarget(
-      this,
-      'Curation-Migration-Backfill-Lambda',
-      {
-        name: `${config.prefix}-backfill-lambda`,
-        eventRule: {
-          description: 'curation migration - event to initiate backfill',
-          //todo: create common event bus
-          //eventBusName: `${config.prefix}-backfill-eventBus`,
-          pattern: {
-            eventType: ['curation-migration-backfill'],
-          },
+    new PocketEventBridgeWithLambdaTarget(this, 'Backfill-Lambda', {
+      name: `${config.prefix}-backfill-lambda`,
+      eventRule: {
+        description: 'curation migration - event to initiate backfill',
+        //todo: create common event bus
+        //eventBusName: `${config.prefix}-backfill-eventBus`,
+        pattern: {
+          eventType: ['curation-migration-backfill'],
         },
-        lambda: {
-          runtime: LAMBDA_RUNTIMES.NODEJS14,
-          handler: 'index.handler',
-          timeout: 120,
-          environment: {
-            REGION: vpc.region,
-            SENTRY_DSN: '',
-            GIT_SHA: '',
-            ENVIRONMENT:
-              config.environment === 'Prod' ? 'production' : 'development',
-          },
-          vpcConfig: {
-            securityGroupIds: vpc.defaultSecurityGroups.ids,
-            subnetIds: vpc.privateSubnetIds,
-          },
-          codeDeploy: {
-            region: vpc.region,
-            accountId: vpc.accountId,
-          },
-          executionPolicyStatements: [
-            {
-              actions: ['secretsmanager:GetSecretValue', 'kms:Decrypt'],
-              resources: [
-                `arn:aws:secretsmanager:${vpc.region}:${vpc.accountId}:secret:CurationToolsDataSync/${config.environment}`,
-                `arn:aws:secretsmanager:${vpc.region}:${vpc.accountId}:secret:CurationToolsDataSync/${config.environment}/*`,
-              ],
-              //todo: add permission to access S3 bucket
-            },
-          ],
-          alarms: {
-            //todo: set alarm
-          },
+      },
+      lambda: {
+        runtime: LAMBDA_RUNTIMES.NODEJS14,
+        handler: 'index.handler',
+        timeout: 120,
+        environment: {
+          REGION: vpc.region,
+          SENTRY_DSN: '',
+          GIT_SHA: '',
+          ENVIRONMENT:
+            config.environment === 'Prod' ? 'production' : 'development',
         },
-        tags: config.tags,
-      }
-    );
+        vpcConfig: {
+          securityGroupIds: vpc.defaultSecurityGroups.ids,
+          subnetIds: vpc.privateSubnetIds,
+        },
+        codeDeploy: {
+          region: vpc.region,
+          accountId: vpc.accountId,
+        },
+        executionPolicyStatements: [
+          {
+            actions: ['secretsmanager:GetSecretValue', 'kms:Decrypt'],
+            resources: [
+              `arn:aws:secretsmanager:${vpc.region}:${vpc.accountId}:secret:CurationToolsDataSync/${config.environment}`,
+              `arn:aws:secretsmanager:${vpc.region}:${vpc.accountId}:secret:CurationToolsDataSync/${config.environment}/*`,
+            ],
+            //todo: add permission to access S3 bucket
+          },
+        ],
+        alarms: {
+          //todo: set alarm
+        },
+      },
+      tags: config.tags,
+    });
   }
 }
