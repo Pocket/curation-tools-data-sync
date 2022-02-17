bc5277d177a61ea47e44be737ba51c21cd7e4d4b
Srilekha Kirsh <skirsh@mozilla.com>
2022-02-17 15:09:13 -0800

    comment apolloClient to check if it fixes graphql build error

--- curation-migration-backfill/externalCaller.spec.ts
+++ curation-migration-backfill/externalCaller.spec.ts
@@ -1,6 +1,7 @@
 import {
   addLiveCurtedItemsToCorpusApi,
   CreateApprovedCuratedCorpusItemInput,
+  getS3UrlFromImageUrl,
 } from './externalCaller';
 
 describe('curatedCorpusApi mutations test', () => {
@@ -16,8 +17,10 @@ describe('curatedCorpusApi mutations test', () => {
       status: 'CORPUS', //todo: check what should be the default for this
       language: `en`,
       publisher: `the guardian`,
-      imageUrl: `https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202018/Health/August/CR-Health-Inlinehero-are-potatoes-good-for-you-0818.jpg`,
+      imageUrl:
+        'https://s3.amazonaws.com/pocket-curatedcorpusapi-dev-images/748ab279-91dd-42be-b72f-95f2ab278c61.jpeg',
       //todo: this is not a s3 url but https://
+      //we need the uploadMutation working
       topic: `FOOD`,
       isCollection: false,
       isTimeSensitive: false,
@@ -28,6 +31,16 @@ describe('curatedCorpusApi mutations test', () => {
     };
 
     const response = await addLiveCurtedItemsToCorpusApi(testData);
+    console.log(response);
+  });
+
+  it('should be able to derive a s3 url from http url', async () => {
+    const testData = {
+      url: 'https://www.consumerreports.org/healthy-eating/are-potatoes-good-for-you/',
+      //todo: we don't have option to add createdAt, updatedAt, createdBy, updatedBy
+    };
+
+    const response = await getS3UrlFromImageUrl(testData.url);
     console.log(response);
   });
 });
--- curation-migration-backfill/externalCaller.ts
+++ curation-migration-backfill/externalCaller.ts
@@ -1,14 +1,8 @@
 import fetch from 'node-fetch';
 import { Blob } from 'buffer';
 import config from './config';
-import {
-  ApolloClient,
-  InMemoryCache,
-  NormalizedCacheObject,
-  HttpLink,
-} from '@apollo/client/core';
-import { gql } from '@apollo/client/core';
-import { createUploadLink } from 'apollo-upload-client';
+//import { gql } from '@apollo/client/core';
+//import { createUploadLink } from 'apollo-upload-client';
 const https = require('https');
 const fs = require('fs');
 
@@ -29,7 +23,12 @@ export type CreateApprovedCuratedCorpusItemInput = {
   scheduledSurfaceGuid: string;
 };
 
-/* wrapper function to call curatedCorpusApi to schedule an item */
+/***
+ * wrapper function to convert
+ *  imageurl -> s3Url
+ *  calls ApprovedItemMutation
+ * @param input
+ */
 export async function addLiveCurtedItemsToCorpusApi(
   input: CreateApprovedCuratedCorpusItemInput
 ) {
@@ -38,65 +37,56 @@ export async function addLiveCurtedItemsToCorpusApi(
     //todo: validate this case with backend products
   }
 
-  const image = await fetchFileFromUrl(input.imageUrl);
+  //uncomment this after fixing image upload
+  //let s3Url = getS3UrlFromImageUrl(input.imageUrl);
+  //input.imageUrl = s3Url;
+
+  return sendApprovedItemMutation(input);
+}
+
+/***
+ * function to convert image url to imageBlob and calls the
+ * uploadImage mutation
+ * @param input
+ */
+export async function getS3UrlFromImageUrl(imageUrl: string) {
+  const image = await fetchFileFromUrl(imageUrl);
 
   if (!image) {
     throw new Error('Failed to download image from source for saving to s3');
   }
 
-  // let downloaded = await downloadFile(input.imageUrl, '/tmp/file');
-  // const data = fs.readFileSync('/tmp/file', 'utf8');
-  // //let file = new File([image], 'filename.zip', { type: 'application/zip' });
-  let s3Url = await generateS3Url('');
+  let s3Url = await generateS3Url(image);
 
-  input.imageUrl = s3Url;
-  return sendApprovedItemMutation(input);
+  return s3Url;
 }
 
-function getClient(): ApolloClient<NormalizedCacheObject> {
-  const cache = new InMemoryCache();
-
-  const client = new ApolloClient({
-    // Provide required constructor fields
-    cache: cache,
-    link: createUploadLink({ uri: config.AdminApi, fetch }),
-    headers: {
-      authorization:
-        'Bearer eyJraWQiOiJremU0TTBDaVhvRE83UWtwaWcxb0gwRjZPSW56Wmc2dWdrMFB5b2pPbHpjPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiUUtxcng4R3V3UWdmNDlMdWVpSUFpUSIsInN1YiI6ImRkMjkzZTNlLTQ4ZjgtNGNjNi05YWQ3LTA5OTM1MmY3YzA4YSIsImNvZ25pdG86Z3JvdXBzIjpbInVzLWVhc3QtMV8xYWxLbHM0cXdfQXV0aDAiXSwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xXzFhbEtsczRxdyIsImNvZ25pdG86dXNlcm5hbWUiOiJBdXRoMF9hZHxNb3ppbGxhLUxEQVB8c2tpcnNoIiwiZ2l2ZW5fbmFtZSI6IlNyaSIsIm5vbmNlIjoiZWFqdS12d…iOjE2NDUxMjY0NzEsImlhdCI6MTY0NTEyMjg3MSwiZmFtaWx5X25hbWUiOiJLaXJzaCIsImp0aSI6ImY1MTk1YTE1LWI0YTEtNDYwNy1iODFhLWY1YTJhM2Y0NmU4YiIsImVtYWlsIjoic2tpcnNoQG1vemlsbGEuY29tIn0.H_utkOUHI2zHum_gIr2Uc19pqAJ0CkOIX6qXcnny6oukFF6WdYgYLIuFvGZXR8ai3q5iXguiO3jl8JilRDzQqZhVdHpWZjajaazFwPGuPJWh_EiivwcTs_0mhbvCqK5aAW4xeB-bNxUSfXyC_nDRnnOWqz4YW2bET89tBTc8EGJgsbVprgGYRxUddmMIS8mAIG_rfKD67jS71A6BQr7OQeOtxlw9uSCg1LagHq-mNV16rAfUFwBp15p_pPkKMRhZ31xShvXi5FA2LO-jpEZFNX8P_I_UtaewzIJcJXVUL5XSDyZxq__0WJ_Uug5MPM4O0Cieigd9GB5mEg9BSfs7xw',
-    },
-    // Provide some optional constructor fields
-    name: 'backfill',
-    version: '1.0',
-    queryDeduplication: false,
-    defaultOptions: {
-      watchQuery: {
-        fetchPolicy: 'cache-and-network',
-      },
-    },
-  });
-
-  return client;
-}
-
-// downloads image from source url
+/***
+ * downloads from the source
+ * @param url
+ */
 const fetchFileFromUrl = async (url: string): Promise<Blob | undefined> => {
   const response = await fetch(url);
   if (response.ok) return response.blob();
 };
 
+/***
+ * function to call uploadApprovedCuratedCorpusItemImage mutation
+ * @param image
+ */
 async function generateS3Url(image: any) {
-  // const UPLOAD_APPROVED_ITEM_IMAGE = gql`
-  //   mutation uploadApprovedCuratedCorpusItemImage($image: Upload!) {
-  //     uploadApprovedCuratedCorpusItemImage(data: $image) {
-  //       url
-  //     }
-  //   }
-  // `;
-  //
-  // const variables = {
-  //   image: image,
-  // };
-  //
+  const UPLOAD_APPROVED_ITEM_IMAGE = `
+    mutation uploadApprovedCuratedCorpusItemImage($image: Upload!) {
+      uploadApprovedCuratedCorpusItemImage(data: $image) {
+        url
+      }
+    }
+  `;
+
+  const variables = {
+    image: image,
+  };
+
   // try {
   //   let response = await getClient().mutate({
   //     mutation: UPLOAD_APPROVED_ITEM_IMAGE,
@@ -106,10 +96,12 @@ async function generateS3Url(image: any) {
   // } catch (error) {
   //   console.log(error);
   // }
-
-  return 'https://s3.amazonaws.com/pocket-curatedcorpusapi-dev-images/748ab279-91dd-42be-b72f-95f2ab278c61.jpeg';
 }
 
+/***
+ * function to call createApprovedCuratedCorpusItem mutation
+ * @param item
+ */
 async function sendApprovedItemMutation(
   item: CreateApprovedCuratedCorpusItemInput
 ) {
@@ -131,6 +123,12 @@ async function sendApprovedItemMutation(
   return sendGraphQLRequest(createApprovedCuratedCorpusItem, variables);
 }
 
+/***
+ * constructs a GRAPHQL request for given query and variables
+ * Note: need to add updated token
+ * @param query
+ * @param variables
+ */
 async function sendGraphQLRequest(query: string, variables: any) {
   let serverUrl = config.AdminApi;
   let t = await fetch(serverUrl, {
@@ -151,23 +149,50 @@ async function sendGraphQLRequest(query: string, variables: any) {
  * @param fileFullPath - the full file path where we want to store our image
  * @return {Promise<>}
  */
-const downloadFile = async (url, fileFullPath) => {
-  console.info('downloading file from url: ' + url);
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, (resp) => {
-        // chunk received from the server
-        resp.on('data', (chunk) => {
-          fs.appendFileSync(fileFullPath, chunk);
-        });
+// const downloadFile = async (url, fileFullPath) => {
+//   console.info('downloading file from url: ' + url);
+//   return new Promise((resolve, reject) => {
+//     https
+//       .get(url, (resp) => {
+//         // chunk received from the server
+//         resp.on('data', (chunk) => {
+//           fs.appendFileSync(fileFullPath, chunk);
+//         });
+//
+//         // last chunk received, we are done
+//         resp.on('end', () => {
+//           resolve('File downloaded and stored at: ' + fileFullPath);
+//         });
+//       })
+//       .on('error', (err) => {
+//         reject(new Error(err.message));
+//       });
+//   });
+// };
 
-        // last chunk received, we are done
-        resp.on('end', () => {
-          resolve('File downloaded and stored at: ' + fileFullPath);
-        });
-      })
-      .on('error', (err) => {
-        reject(new Error(err.message));
-      });
-  });
-};
+// /***
+//  * generates apollo client
+//  */
+// function getClient(): ApolloClient<NormalizedCacheObject> {
+//   const cache = new InMemoryCache();
+//   const client = new ApolloClient({
+//     // Provide required constructor fields
+//     cache: cache,
+//     //link: createUploadLink({ uri: config.AdminApi, fetch }),
+//     headers: {
+//       authorization:
+//         'Bearer eyJraWQiOiJremU0TTBDaVhvRE83UWtwaWcxb0gwRjZPSW56Wmc2dWdrMFB5b2pPbHpjPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoia0FsUnpWVGpSX2VBRElKb3FtSXU5ZyIsInN1YiI6ImRkMjkzZTNlLTQ4ZjgtNGNjNi05YWQ3LTA5OTM1MmY3YzA4YSIsImNvZ25pdG86Z3JvdXBzIjpbInVzLWVhc3QtMV8xYWxLbHM0cXdfQXV0aDAiXSwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xXzFhbEtsczRxdyIsImNvZ25pdG86dXNlcm5hbWUiOiJBdXRoMF9hZHxNb3ppbGxhLUxEQVB8c2tpcnNoIiwiZ2l2ZW5fbmFtZSI6IlNyaSIsIm5vbmNlIjoiUzlRTjNoUFJaWFdDR1Y3dHhsaTh5c2gxa3lXZ3Nlb05YVlZEZDRhVG1jYzN5aUJjSUtxZEhPUGNHWmxKaDBXdWJwSWdCeWFTR3BfUDZuSE5fdFhRcEZnNmhkb1hudW1MQy02b25PakpINzVDdDJLR0ZDX1VIX00tM1oyekt6T05KQjJqVnB2VHhHdWF2WnBuQUQ3UFpNdTNxNjB0QTFFYUZyQkpIejJybTd3IiwiY3VzdG9tOmdyb3VwcyI6IltcImV2ZXJ5b25lXCIsXCJJbnRyYW5ldFdpa2lcIixcIlN0YXRzRGFzaGJvYXJkXCIsXCJhbGxfbGRhcF91c2Vyc1wiLFwiZWdlbmNpYV9jYVwiLFwiZWdlbmNpYV91c1wiLFwib2ZmaWNlX3JlbW90ZVwiLFwicGhvbmVib29rX2FjY2Vzc1wiLFwicG9ja2V0X3ZwblwiLFwidGVhbV9tb2NvXCIsXCJ0ZWFtX21vY29fYmVuZWZpdGVkXCIsXCJ0ZWFtX3BvY2tldFwiLFwidnBuX2NvcnBcIixcInZwbl9kZWZhdWx0XCIsXCJtb3ppbGxpYW5zb3JnX2ppcmFfY2xvdWRfYWNjZXNzXCIsXCJtb3ppbGxpYW5zb3JnX3BvY2tldF9iYWNrZW5kXCIsXCJtb3ppbGxpYW5zb3JnX3BvY2tldF9kYXRhYW5hbHl0aWNzXCIsXCJtb3ppbGxpYW5zb3JnX3BvY2tldF9mZWF0dXJlZmxhZ3NcIixcIm1vemlsbGlhbnNvcmdfcG9ja2V0X3JlYWRvbmx5XCIsXCJtb3ppbGxpYW5zb3JnX3BvY2tldF9zY2hlZHVsZWRfc3VyZmFjZV9jdXJhdG9yX2Z1bGxcIl0iLCJwaWN0dXJlIjoiaHR0cHM6XC9cL3MuZ3JhdmF0YXIuY29tXC9hdmF0YXJcL2YwMDI0NTEzMjU0MDExYWU0NTUwNzU0NzVhMmFlZTc5P3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGc2sucG5nIiwib3JpZ2luX2p0aSI6ImQ1ZWRmZDA4LTJlZDUtNDdiOS05OTQxLTg5OTY4N2E5NDNmMSIsImF1ZCI6IjZxdDk0czlkNjUxazI0bXZ1bDlxMmxicmN2IiwiaWRlbnRpdGllcyI6W3sidXNlcklkIjoiYWR8TW96aWxsYS1MREFQfHNraXJzaCIsInByb3ZpZGVyTmFtZSI6IkF1dGgwIiwicHJvdmlkZXJUeXBlIjoiT0lEQyIsImlzc3VlciI6bnVsbCwicHJpbWFyeSI6InRydWUiLCJkYXRlQ3JlYXRlZCI6IjE2MzQwNzYzNjU3OTEifV0sInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjQ1MTMzNjc5LCJuYW1lIjoiU3JpIEtpcnNoIiwibmlja25hbWUiOiJTcmkgS2lyc2giLCJleHAiOjE2NDUxMzcyNzksImlhdCI6MTY0NTEzMzY3OSwiZmFtaWx5X25hbWUiOiJLaXJzaCIsImp0aSI6ImU1YmIxOWMyLTFlNTctNDVmNC04N2JhLTA4Yjg4MTljN2Q0YSIsImVtYWlsIjoic2tpcnNoQG1vemlsbGEuY29tIn0.Xji7vbK3HdpAHG_MA8nJhZ2out4E8ILhdAtuBMlxg0aWpB6tjQvzCQaJ5Sa5hsxT8fJh3-FDuf-rmrwo2zj1YZjr_TCzI5jizkREb-Edc11kVroFelVuZV_vJL1S155y4rUuUiSnSuHmeC80ZLExymNieTNRf3-5otYwTXaypoQ3uKIPDYvWHzBvg86a068ItMtjZnt_SLCe2Zc7__EzZkAU6xGGhGTFqwI3qTUY14JgiruKMPrEC7Znn2flJbEJT1Wyc7Qw5-2lXMIQh8t5rgYYRHiO0x7kHlBPbKYbWiQJ1svj-6q99JXGh11gURJTxPmYEdrbdeJjD8XoxPeznQ',
+//     },
+//     // Provide some optional constructor fields
+//     name: 'backfill',
+//     version: '1.0',
+//     queryDeduplication: false,
+//     defaultOptions: {
+//       watchQuery: {
+//         fetchPolicy: 'cache-and-network',
+//       },
+//     },
+//   });
+//
+//   return client;
+// }
