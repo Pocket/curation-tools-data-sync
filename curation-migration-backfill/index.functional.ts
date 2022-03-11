22fe3a45bb6d2f25d01fd57afe1f646420cf6ee4
Srilekha Kirsh <skirsh@mozilla.com>
2022-03-11 15:11:52 -0800

    fix failing test

--- curation-migration-backfill/index.functional.ts
+++ curation-migration-backfill/index.functional.ts
@@ -1,5 +1,23 @@
+import { handlerFn } from './index';
+import { CuratedItemRecord } from './dynamodb/types';
+import { getByCuratedRecId, truncateDb } from './dynamodb/dynamoUtilities';
+import { dbClient } from './dynamodb/dynamoDbClient';
+
 describe('test placeholder', () => {
-  it('sample test', async () => {
-    expect(true).toBe(true);
+  afterAll(async () => {
+    await truncateDb(dbClient);
+  });
+
+  //todo: can be removed later when we remove the dynamo check in index.ts
+  it('when event type is dynamo, insert record', async () => {
+    await handlerFn('dynamo');
+    const res: CuratedItemRecord = await getByCuratedRecId(dbClient, 10);
+
+    console.log(res);
+    expect(res).not.toBeUndefined();
+    expect(res?.curatedRecId).toEqual(10);
+    expect(res?.approvedItemExternalId).toEqual('random-approved-guid-10');
+    expect(res?.scheduledItemExternalId).toEqual('random-scheduled-guid-10');
+    expect(res?.scheduledSurfaceGuid).toEqual('NEW_TAB_EN_US');
   });
 });
--- curation-migration-backfill/index.spec.ts
+++ curation-migration-backfill/index.spec.ts
@@ -1,22 +1,5 @@
-import { handlerFn } from './index';
-import { CuratedItemRecord } from './dynamodb/types';
-import { getByCuratedRecId, truncateDb } from './dynamodb/dynamoUtilities';
-import { dbClient } from './dynamodb/dynamoDbClient';
-
 describe('test placeholder', () => {
-  afterAll(async () => {
-    await truncateDb(dbClient);
-  });
-
-  it('when event type is dynamo, insert record', async () => {
-    await handlerFn('dynamo');
-    const res: CuratedItemRecord = await getByCuratedRecId(dbClient, 10);
-
-    console.log(res);
-    expect(res).not.toBeUndefined();
-    expect(res?.curatedRecId).toEqual(10);
-    expect(res?.approvedItemExternalId).toEqual('random-approved-guid-10');
-    expect(res?.scheduledItemExternalId).toEqual('random-scheduled-guid-10');
-    expect(res?.scheduledSurfaceGuid).toEqual('NEW_TAB_EN_US');
+  it('sample test', async () => {
+    expect(true).toBe(true);
   });
 });
