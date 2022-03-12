2793a49da69e0e2378f7319df41ae6b6099f975b
Srilekha Kirsh <skirsh@mozilla.com>
2022-03-11 16:17:24 -0800

    circle ci setup for integration test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|functional|integration).ts'],
+  testMatch: ['**/?(*.)+(spec|integration).ts'],
   testPathIgnorePatterns: ['/dist/'],
   setupFiles: ['./jest.setup.js'],
 };
