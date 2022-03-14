ebec665f122d87f40ceb6eb32e4a6bc6f04e520f
Kat Schelonka <kschelonka@mozilla.com>
2022-03-14 09:32:17 -0700

    feat(backfill): data transforms to call load mutation
    INFRA-336

--- curation-migration-backfill/index.spec.ts
+++ curation-migration-backfill/index.spec.ts
@@ -1,5 +1,127 @@
-describe('test placeholder', () => {
-  it('sample test', async () => {
-    expect(true).toBe(true);
+import { epochToDateString, handlerFn } from './';
+import nock from 'nock';
+import config from './config';
+import { SQSEvent } from 'aws-lambda';
+
+describe('curation migration', () => {
+  const record = {
+    curated_rec_id: '123',
+    time_live: 1647042571,
+    time_added: 1647042571,
+    time_updated: 999999999,
+    title: 'Equine dentist wins lottery',
+    excerpt: `"It's kind of against my job description," said Dr. Orsteeth, "but this is definitely a case where I don't want to look a gift horse in the mouth!"`,
+    curator: 'btownie',
+    image_src: 'https://some-image.png',
+    resolved_id: '123',
+    resolved_url: 'https://yougotgums.com',
+    lang: 'en',
+    topic_name: 'entertainment',
+    feed_id: 8,
+    slug: 'en-intl',
+  };
+  describe('epochToDateString', () => {
+    it('works for for zero-padded months', async () => {
+      const date = 1647042571; // 2022-03-11 23:49:31 UTC
+      expect(epochToDateString(date)).toEqual('2022-03-11');
+    });
+    it('works for two-digit months', async () => {
+      const date = 1671032431; // 2022-12-14 15:40:31 UTC
+      expect(epochToDateString(date)).toEqual('2022-12-14');
+    });
+  });
+  describe('lambda handler', () => {
+    it('formats record as expected', async () => {
+      nock(config.AdminApi)
+        .post('/')
+        .reply(200, {
+          data: {
+            getUrlMetadata: {
+              isSyndicated: true,
+              isCollection: false,
+              publisher: 'Gums Weekly',
+            },
+          },
+        });
+
+      const fakeEvent = {
+        Records: [{ body: JSON.stringify(record) }],
+      } as unknown as SQSEvent;
+      const actual = await handlerFn(fakeEvent);
+      expect(actual).toEqual([
+        {
+          url: 'https://yougotgums.com',
+          title: 'Equine dentist wins lottery',
+          excerpt: `"It's kind of against my job description," said Dr. Orsteeth, "but this is definitely a case where I don't want to look a gift horse in the mouth!"`,
+          status: 'RECOMMENDATION',
+          language: 'en',
+          publisher: 'Gums Weekly',
+          imageUrl: 'https://some-image.png',
+          topic: 'entertainment',
+          source: 'BACKFILL',
+          isCollection: false,
+          isSyndicated: true,
+          createdAt: 1647042571,
+          updatedAt: 1647042571,
+          createdBy: 'ad|Mozilla-LDAP|btownie',
+          updatedBy: 'ad|Mozilla-LDAP|btownie',
+          scheduledDate: '2022-03-11',
+          scheduledSurfaceGuid: 'NEW_TAB_EN_INTL',
+        },
+      ]);
+    });
+    it('throws an error if prospect-api has error', async () => {
+      nock(config.AdminApi)
+        .post('/')
+        .reply(200, {
+          data: {
+            getUrlMetadata: {
+              isSyndicated: true,
+              isCollection: false,
+              publisher: 'Gums Weekly',
+            },
+          },
+        })
+        .post('/')
+        .reply(200, { errors: [{ message: 'server bork' }] });
+      const fakeEvent = {
+        Records: [
+          { body: JSON.stringify(record) },
+          { body: JSON.stringify(record) },
+        ],
+      } as unknown as SQSEvent;
+      await expect(() => handlerFn(fakeEvent)).rejects.toThrowError(
+        'Failed to retrieve data from prospect-api for url https://yougotgums.com'
+      );
+    });
+    it('throws an error if prospect-api returns null data', async () => {
+      nock(config.AdminApi).post('/').reply(200, { data: null });
+      const fakeEvent = {
+        Records: [{ body: JSON.stringify(record) }],
+      } as unknown as SQSEvent;
+      await expect(() => handlerFn(fakeEvent)).rejects.toThrowError(
+        'Failed to retrieve data from prospect-api for url https://yougotgums.com'
+      );
+    });
+    it('sets curator to "unknown" if null', async () => {
+      const anotherRecord = { ...record, curator: null };
+      nock(config.AdminApi)
+        .post('/')
+        .reply(200, {
+          data: {
+            getUrlMetadata: {
+              isSyndicated: true,
+              isCollection: false,
+              publisher: 'Gums Weekly',
+            },
+          },
+        });
+      const fakeEvent = {
+        Records: [{ body: JSON.stringify(anotherRecord) }],
+      } as unknown as SQSEvent;
+      const actual = await handlerFn(fakeEvent);
+      expect(actual[0].createdBy).toEqual('unknown');
+      expect(actual[0].updatedBy).toEqual('unknown');
+    });
   });
 });
--- curation-migration-backfill/index.ts
+++ curation-migration-backfill/index.ts
@@ -1,8 +1,132 @@
 import * as Sentry from '@sentry/serverless';
 import config from './config';
+import { SQSEvent } from 'aws-lambda';
+import fetch from 'node-fetch';
+import { setTimeout } from 'timers/promises';
 
 export enum EVENT {
   CURATION_MIGRATION_BACKFILL = 'curation-migration-backfill',
+}
+
+interface BackfillMessage {
+  curated_rec_id: string;
+  time_live: number;
+  time_added: number;
+  time_updated: number;
+  title: string;
+  excerpt: string;
+  curator: string | null;
+  image_src: string;
+  resolved_id: number;
+  resolved_url: string;
+  lang: string;
+  topic_name: string | null;
+  feed_id: number;
+  slug: string;
+}
+
+interface CorpusInput {
+  url: string;
+  title: string;
+  excerpt: string;
+  status: 'RECOMMENDATION'; // ?
+  language: string;
+  publisher: string;
+  imageUrl: string;
+  topic: string | null;
+  source: 'BACKFILL';
+  isCollection: boolean;
+  isSyndicated: boolean;
+  createdAt: number;
+  createdBy: string | null;
+  updatedAt: number;
+  updatedBy: string | null;
+  scheduledDate: string; // YYYY-MM-DD
+  scheduledSurfaceGuid: string;
+}
+
+type ProspectInfo = Pick<
+  CorpusInput,
+  'isCollection' | 'isSyndicated' | 'publisher'
+>;
+
+// Some data mappings
+const feedIdToGuid = {
+  1: 'NEW_TAB_EN_US',
+  3: 'NEW_TAB_DE_DE',
+  6: 'NEW_TAB_EN_GB',
+  8: 'NEW_TAB_EN_INTL',
+};
+
+const ssoPrefix = 'ad|Mozilla-LDAP|';
+
+/**
+ * Convert epoch to UTC date (YYYY-MM-DD).
+ */
+export function epochToDateString(epoch: number): string {
+  const date = new Date(epoch * 1000);
+  const month = date.getUTCMonth() + 1; // zero-indexed
+  const padMonthString = month.toString().padStart(2, '0');
+  return `${date.getUTCFullYear()}-${padMonthString}-${date.getUTCDate()}`;
+}
+
+async function hydrateCorpusInput(
+  record: BackfillMessage
+): Promise<CorpusInput> {
+  const prospectData = await fetchProspectData(record.resolved_url);
+  const curator = record.curator ? `${ssoPrefix}${record.curator}` : 'unknown';
+  const corpusInput = {
+    url: record.resolved_url,
+    title: record.title,
+    excerpt: record.excerpt,
+    status: 'RECOMMENDATION' as const,
+    language: record.lang,
+    imageUrl: record.image_src,
+    topic: record.topic_name,
+    source: 'BACKFILL' as const,
+    createdAt: record.time_added,
+    updatedAt: record.time_added, // dropping time_updated since we don't know updatedBy
+    createdBy: curator,
+    updatedBy: curator,
+    scheduledDate: epochToDateString(record.time_live),
+    scheduledSurfaceGuid: feedIdToGuid[record.feed_id],
+    ...prospectData,
+  };
+  return corpusInput;
+}
+
+/**
+ * Retrieve metadata from prospect-api request
+ * @param url the URL key for retrieving metadata from prospect-api
+ * @returns Promise<ProspectInfo>
+ */
+async function fetchProspectData(url: string): Promise<ProspectInfo> {
+  const query = `
+  query getUrlMetadata($url: String!) {
+    getUrlMetadata(url: $url) {
+      isSyndicated
+      isCollection
+      publisher
+    }
+  }
+  `;
+  const variables = { url };
+  const res = await fetch(config.AdminApi, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+  const jsonRes = await res.json();
+  if (jsonRes.errors != null || jsonRes.data == null) {
+    throw new Error(
+      `Failed to retrieve data from prospect-api for url ${url}.\nErrors: ${JSON.stringify(
+        jsonRes.errors
+      )}`
+    );
+  }
+  return jsonRes.data.getUrlMetadata as ProspectInfo;
 }
 
 /**
@@ -10,9 +134,23 @@ export enum EVENT {
  * to make unit-testing easier.
  * Takes event from cloudwatch to initiatie the migration
  */
-export async function handlerFn(event: any) {
-  console.log(JSON.stringify(event));
-  Sentry.captureMessage(`testing sentry -> ` + JSON.stringify(event));
+export async function handlerFn(event: SQSEvent) {
+  // Not using map since we want to block after each record
+  const res = Array(event.Records.length);
+  let index = 0;
+  for await (const record of event.Records) {
+    const message: BackfillMessage = JSON.parse(record.body);
+    const corpusInput = await hydrateCorpusInput(message);
+    // Wait a sec... don't barrage the api. We're just backfilling here.
+    await setTimeout(1000);
+    // Here's where you'd call the mutation instead of adding data to array
+    // TODO
+    res[index] = corpusInput;
+    index += 1;
+  }
+  // Can remove this once we actually call the mutation
+  // And update the tests to check the arguments instead
+  return res;
 }
 
 Sentry.AWSLambda.init({
