ad9d2a21bbc2235f1666b00e374b46fc1d19e8ff
Jonathan Petto <jpetto@users.noreply.github.com>
2022-03-16 14:43:53 -0500

    - finish up jwt generator - add spec test

--- curation-migration-backfill/jwt.ts
+++ curation-migration-backfill/jwt.ts
@@ -1,14 +1,16 @@
 import jwt from 'jsonwebtoken';
 import jwkToPem from 'jwk-to-pem';
-import config from './config';
 
+// this is the required subset of properties required by the admin-api gateway
+// https://github.com/Pocket/admin-api/blob/a0bb468cece3ba5bc1a00e1098652a49d433a81d/src/jwtUtils.ts#L98
 type JwtPayload = {
   iss: string;
   aud: string;
   iat: number; //timestamp
   exp: number;
-  sub: string;
-  apiId?: string;
+  name: string;
+  'custom:groups': string; // json enconded string - array of groups
+  identities: { userId: string }[];
 };
 
 /**
@@ -17,21 +19,25 @@ type JwtPayload = {
  * @param fxaId
  * https://www.npmjs.com/package/jsonwebtoken
  */
-export function generateJwt(privateKey, fxaId: string) {
+export function generateJwt(privateKey) {
   const now = Math.round(Date.now() / 1000);
 
   const payload: JwtPayload = {
-    iss: config.jwt.iss,
-    aud: config.jwt.aud,
+    iss: 'https://getpocket.com',
+    aud: '',
     iat: now,
-    exp: now + 60 * 10, //expires in 10 mins
-    sub: fxaId,
-    apiId: config.app.apiId,
+    exp: now + 60 * 5, //expires in 5 mins
+    name: 'Backfill McUser',
+    identities: [{ userId: 'backfill-user' }],
+    // this group gives us full access in corpus API
+    'custom:groups': JSON.stringify([
+      'mozilliansorg_pocket_scheduled_surface_curator_full',
+    ]),
   };
 
   return jwt.sign(payload, jwkToPem(privateKey, { private: true }), {
     algorithm: 'RS256',
-    // Required by client-api to disambiguate from other key(s)
+    // Required by admin-api to disambiguate from other key(s)
     keyid: privateKey.kid,
   });
 }
