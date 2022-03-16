4a2487bc1287777e5ce8c1732340b87ee694fcba
Jonathan Petto (they/them) <jpetto@users.noreply.github.com>
2022-03-16 18:39:42 -0500

    feat(backfill): add JWT generator to send JWT to admin-api with all requests. (#64)
    * feat: generate JWT for request to admin-api
    
    * - finish up jwt generator
    - add spec test
    
    * updates:
    
    - move jwt constants to config
    - update jwt generator & test w/config values
    - copy secrets manager from fxa
    
    * change function name in secrets manager
    
    * re-add domainPrefix
    
    * Update curation-migration-backfill/jwt.spec.ts
    
    remove console.log copied from fxa
    
    Co-authored-by: Kelvin Yeboah <kelvinkyeboah@gmail.com>
    
    * lint-fix
    
    Co-authored-by: Kelvin Yeboah <kyeboah@getpocket.com>
    Co-authored-by: Kelvin Yeboah <kelvinkyeboah@gmail.com>
    Co-authored-by: Srilekha Kirsh <skirsh@mozilla.com>

--- curation-migration-backfill/config.ts
+++ curation-migration-backfill/config.ts
@@ -26,6 +26,14 @@ const config = {
   AdminApi: isDev
     ? process.env.CLIENT_API_URI || 'https://admin-api.getpocket.dev'
     : process.env.CLIENT_API_URI || 'https://admin-api.readitlater.com',
+  jwt: {
+    key: process.env.JWT_KEY || 'CurationToolsDataSync/Dev/JWT_KEY',
+    iss: process.env.JWT_ISS || 'https://getpocket.com',
+    aud: process.env.JWT_AUD || 'https://admin-api.getpocket.com/',
+    name: 'B. Ackfil User',
+    userId: 'backfill-user',
+    groups: ['mozilliansorg_pocket_scheduled_surface_curator_full'],
+  },
 };
 
 export default config;
