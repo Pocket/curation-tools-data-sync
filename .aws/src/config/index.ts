a35f835e4ba3619dd6ae2725b6ab2d3d6f3cc43e
Jonathan Petto <jpetto@users.noreply.github.com>
2022-03-16 16:23:48 -0500

    re-add domainPrefix

--- .aws/src/config/index.ts
+++ .aws/src/config/index.ts
@@ -1,10 +1,12 @@
 const name = 'CurationToolsDataSync';
+const domainPrefix = 'curation-tools-data-sync';
 const isDev = process.env.NODE_ENV === 'development';
 const environment = isDev ? 'Dev' : 'Prod';
 
 export const config = {
   name,
   isDev,
+  domainPrefix,
   prefix: `${name}-${environment}`,
   circleCIPrefix: `/${name}/CircleCI/${environment}`,
   shortName: 'CTSYNC',
