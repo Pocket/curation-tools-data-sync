17be6f0b5ab63a697fcd7876c28aefb1bcc05a39
Srilekha Kirsh <skirsh@mozilla.com>
2022-03-22 20:19:12 -0700

    nit

--- curation-migration-backfill/index.spec.ts
+++ curation-migration-backfill/index.spec.ts
@@ -26,11 +26,6 @@ describe('curation migration', () => {
     slug: 'en-intl',
   };
 
-  const record2 = {
-    curated_rec_id: '456',
-    resolved_url: 'http://failthiscall.com',
-  };
-
   beforeEach(() => {
     // mock the secrets manager call
     sinon
@@ -47,58 +42,6 @@ describe('curation migration', () => {
   });
 
   describe('lambda handler', () => {
-    it('formats record as expected', async () => {
-      nock(config.AdminApi)
-        .post('/')
-        .reply(200, {
-          data: {
-            getUrlMetadata: {
-              isSyndicated: true,
-              isCollection: false,
-              publisher: 'Gums Weekly',
-            },
-          },
-        })
-        .post('/') //curated-corpus-api call for first event
-        .reply(200, {
-          data: {
-            importApprovedCuratedCorpusItem: {
-              approvedItem: {
-                externalId: 'random-approvedItem-guid',
-              },
-              scheduledItem: {
-                externalId: 'random-scheduledItem-guid',
-                scheduledSurfaceGuid: 'NEW_TAB_EN_US',
-              },
-            },
-          },
-        });
-
-      const fakeEvent = {
-        Records: [{ body: JSON.stringify(record) }],
-      } as unknown as SQSEvent;
-      await handlerFn(fakeEvent);
-      //   {
-      //     url: 'https://yougotgums.com',
-      //     title: 'Equine dentist wins lottery',
-      //     excerpt: `"It's kind of against my job description," said Dr. Orsteeth, "but this is definitely a case where I don't want to look a gift horse in the mouth!"`,
-      //     status: 'RECOMMENDATION',
-      //     language: 'EN',
-      //     publisher: 'Gums Weekly',
-      //     imageUrl: 'https://some-image.png',
-      //     topic: 'entertainment',
-      //     source: 'BACKFILL',
-      //     isCollection: false,
-      //     isSyndicated: true,
-      //     createdAt: 1647042571,
-      //     updatedAt: 1647042572,
-      //     createdBy: 'ad|Mozilla-LDAP|cohara',
-      //     updatedBy: 'ad|Mozilla-LDAP|cohara',
-      //     scheduledDate: '2022-03-11',
-      //     scheduledSurfaceGuid: 'NEW_TAB_EN_INTL',
-      //   },
-      // );
-    });
     it('returns batch item failure if prospect-api has error, with partial success', async () => {
       nock(config.AdminApi)
         .post('/') //prospect-api call for first event
@@ -186,10 +129,8 @@ describe('curation migration', () => {
     });
 
     it('returns no batch item failures if curated-corpus-api request is successful', async () => {
-      // mock the first request to prospect-api to be successful.
-      // the second request (post) is made to curated-corpus-api
-      // both requests are times 2 since we are testing for two sqs events
-
+      //when both prospectApi and curatedCorpusApi call returns success,
+      // no batch item should fail
       sinon.stub(ProspectApi, 'fetchProspectData').returns(
         Promise.resolve({
           isSyndicated: true,
@@ -234,6 +175,25 @@ describe('curation migration', () => {
     it('returns batch item failure if curated-corpus-api request throws an error', async () => {
       // mock the first request to prospect-api to be successful
       // the second request (post) is made to curated-corpus-api, let's fail that one with a graphql error
+
+      //for the first batch item
+      nock(config.AdminApi)
+        .post('/')
+        .reply(200, {
+          data: {
+            getUrlMetadata: {
+              isSyndicated: true,
+              isCollection: false,
+              publisher: 'Gums Weekly',
+            },
+          },
+        })
+        .post('/')
+        .reply(200, {
+          errors: [{ message: 'test-graphql-error' }],
+        });
+
+      //for the second batch item
       nock(config.AdminApi)
         .post('/')
         .reply(200, {
