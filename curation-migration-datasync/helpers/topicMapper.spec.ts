8a13909e78bc78d393bc3107e7f12df7c549c58e
Sri Kirsh <skirsh@mozilla.com>
2022-03-30 14:43:48 -0700

    Update curation-migration-datasync/helpers/topicMapper.spec.ts
    Co-authored-by: Kat Schelonka <34227334+kschelonka@users.noreply.github.com>

--- curation-migration-datasync/helpers/topicMapper.spec.ts
+++ curation-migration-datasync/helpers/topicMapper.spec.ts
@@ -1,7 +1,7 @@
 import { getTopicForReaditLaTmpDatabase } from './topicMapper';
 import { expect } from 'chai';
 
-describe('topics mapepr test', () => {
+describe('topics mapper test', () => {
   it('should convert a valid topic from curatedCorpusApi to readitLaTmp topics', () => {
     const curatedCorpusTopic = 'HEALTH_FITNESS';
     expect(getTopicForReaditLaTmpDatabase(curatedCorpusTopic)).equals(
