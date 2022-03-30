ba4b1082c2a44b26608c3d4e9b4a6da6b7bed41d
Srilekha Kirsh <skirsh@mozilla.com>
2022-03-30 09:06:44 -0700

    adding assertions

--- curation-migration-datasync/index.integration.ts
+++ curation-migration-datasync/index.integration.ts
@@ -1,7 +1,10 @@
 import { CuratedItemRecord, ScheduledSurfaceGuid } from './dynamodb/types';
 import { truncateDynamoDb } from './dynamodb/dynamoUtilities';
 import { dbClient } from './dynamodb/dynamoDbClient';
-import { insertCuratedItem } from './dynamodb/curatedItemIdMapper';
+import {
+  getByScheduledItemExternalId,
+  insertCuratedItem,
+} from './dynamodb/curatedItemIdMapper';
 import * as SecretManager from '../curation-migration-datasync/secretManager';
 import sinon from 'sinon';
 import { getDbCredentials } from '../curation-migration-datasync/secretManager';
@@ -10,19 +13,14 @@ import { AddScheduledItemPayload, EventDetailType } from './types';
 import { handlerFn } from './index';
 import nock from 'nock';
 import config from './config';
+import { Knex } from 'knex';
+import { convertDateToTimestamp } from './eventConsumer';
 
 describe('event consumption integration test', function () {
   const timestamp1 = Math.round(new Date('2020-10-10').getTime() / 1000);
   const timestamp2 = Math.round(new Date('2021-10-10').getTime() / 1000);
 
   const curatedItemRecords: CuratedItemRecord[] = [
-    {
-      curatedRecId: 1,
-      scheduledSurfaceGuid: ScheduledSurfaceGuid.NEW_TAB_EN_GB,
-      scheduledItemExternalId: 'random_scheduled_guid_1',
-      approvedItemExternalId: 'random_approved_guid_1',
-      lastUpdatedAt: Math.round(new Date().getTime() / 1000),
-    },
     {
       curatedRecId: 2,
       scheduledSurfaceGuid: ScheduledSurfaceGuid.NEW_TAB_EN_US,
@@ -124,6 +122,20 @@ describe('event consumption integration test', function () {
     scheduledDate: '2022-03-25',
   };
 
+  let testEvent = {
+    version: '0',
+    id: '8afc769f-bf1e-c87a-a0b6-1aa4c526831f',
+    'detail-type': 'add-scheduled-item',
+    source: 'curation-tools-datasync',
+    account: '124567',
+    time: '2022-03-29T22:55:16Z',
+    region: 'us-east-1',
+    resources: [],
+    detail: {
+      ...testEventBody,
+    },
+  };
+
   it('adds non-syndicated articles', async () => {
     const parserData = { resolved_id: '23434', item: { domain_id: '123' } };
     const params = new URLSearchParams({
@@ -141,26 +153,76 @@ describe('event consumption integration test', function () {
       .get('/' + params.toString())
       .reply(200, parserData);
 
-    let testEvent = {
-      version: '0',
-      id: '8afc769f-bf1e-c87a-a0b6-1aa4c526831f',
-      'detail-type': 'add-scheduled-item',
-      source: 'curation-tools-datasync',
-      account: '124567',
-      time: '2022-03-29T22:55:16Z',
-      region: 'us-east-1',
-      resources: [],
-      detail: {
-        ...testEventBody,
-      },
-    };
-
     await handlerFn(testEvent);
-
-    //todo: assert if record is present in rds and in dynamo
+    await assertTables(testEventBody, db);
   });
 
   it('adds syndicated articles', () => {});
 
   it('throws error when scheduledItem externalId is not present', () => {});
 });
+
+async function assertTables(testEventBody: AddScheduledItemPayload, db: Knex) {
+  let curatedItemRecord = await getByScheduledItemExternalId(
+    dbClient,
+    'random_scheduled_guid_1'
+  );
+  console.log(curatedItemRecord);
+  const curatedItem = await db('curated_feed_items')
+    .select()
+    .where({
+      curated_rec_id: curatedItemRecord[0].curatedRecId,
+    })
+    .first();
+
+  expect(curatedItem.time_live).toEqual(
+    convertDateToTimestamp(testEventBody.scheduledDate)
+  );
+  expect(curatedItem.feed_id).toEqual(1);
+  expect(curatedItem.resolved_id).toEqual(23434);
+
+  const queuedItems = await db('curated_feed_queued_items')
+    .select()
+    .where({
+      queued_id: curatedItem.queued_id,
+    })
+    .first();
+
+  expect(queuedItems.feed_id).toEqual(1);
+  expect(queuedItems.resolved_id).toEqual(23434);
+  expect(queuedItems.curator).toEqual('sri');
+  expect(queuedItems.relevance_length).toEqual('week');
+  expect(queuedItems.topic_id).toEqual(4);
+  expect(queuedItems.time_added).toEqual(testEventBody.createdAt);
+  expect(queuedItems.time_updated).toEqual(testEventBody.updatedAt);
+  expect(queuedItems.prospect_id).toEqual(curatedItem.prospect_id);
+
+  const prospectItem = await db('curated_feed_prospects')
+    .select()
+    .where({
+      prospect_id: curatedItem.prospect_id,
+    })
+    .first();
+
+  expect(prospectItem.feed_id).toEqual(1);
+  expect(prospectItem.resolved_id).toEqual(23434);
+  expect(prospectItem.type).toBeNull();
+  expect(prospectItem.curator).toEqual('sri');
+  expect(prospectItem.status).toEqual('ready');
+  expect(prospectItem.top_domain_id).toEqual(123);
+  expect(prospectItem.time_added).toEqual(testEventBody.createdAt);
+  expect(prospectItem.time_updated).toEqual(testEventBody.updatedAt);
+  expect(prospectItem.prospect_id).toEqual(curatedItem.prospect_id);
+  expect(prospectItem.title).toEqual(testEventBody.title);
+  expect(prospectItem.excerpt).toEqual(testEventBody.excerpt);
+  expect(prospectItem.image_src).toEqual(testEventBody.imageUrl);
+
+  const tileSource = await db('tile_source')
+    .select()
+    .where({
+      source_id: curatedItemRecord[0].curatedRecId,
+    })
+    .first();
+
+  expect(tileSource.tile_id).toBeGreaterThan(0);
+}
