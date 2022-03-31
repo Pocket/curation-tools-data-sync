77e284897bfa0887ce9cef97aa7b1d36d72ec200
Srilekha Kirsh <skirsh@mozilla.com>
2022-03-31 09:52:45 -0700

    parser backoff with max 3 retry

--- curation-migration-datasync/externalCaller/parser.ts
+++ curation-migration-datasync/externalCaller/parser.ts
@@ -1,30 +1,24 @@
-import fetch from 'node-fetch';
-import config from '../config';
-
-type ParserMetadataResponse = {
-  resolvedId: string;
-  domainId: string;
-};
+import { backOff } from 'exponential-backoff';
+import { parserCaller } from './parserCaller';
 
 /**
- * Fetch domainID and resolvedID from the parser service,
- * using the URL.
- * @param url the URL to fetch data for
- * @returns ParserMetadataResponse
+ * calls parser to fetch domainID and resolvedId.
+ * Retries for max 3 times with backoff.
+ * https://www.npmjs.com/package/exponential-backoff#ibackoffoptions
+ * @param url
  */
-export async function getParserMetadata(
-  url: string
-): Promise<ParserMetadataResponse> {
-  const params = new URLSearchParams({
-    output: 'regular',
-    getItem: '1',
-    images: '0',
-    url: url,
-  });
-  const res = await fetch(config.parserEndpoint + '/' + params.toString());
-  const jsonRes = await res.json();
-  return {
-    domainId: jsonRes['item']['domain_id'],
-    resolvedId: jsonRes['resolved_id'],
+export async function getParserMetadata(url: string): Promise<any> {
+  const backOffOptions = {
+    numOfAttempts: 3, //default is 10
   };
+
+  //backoff doesn't allow us to type the response, but its enforced in parserCaller()
+  let res: any;
+  try {
+    res = await backOff(() => parserCaller(url), backOffOptions);
+    console.log(res);
+  } catch (e) {
+    throw new Error(e);
+  }
+  return res;
 }
