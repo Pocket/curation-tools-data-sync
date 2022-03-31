d5a639baf116106bd8a3208626bd1d51828d4fa2
Sri Kirsh <skirsh@mozilla.com>
2022-03-31 12:01:40 -0700

    chore(parser): add exponential backoff with max 3 retry for parser  (#106)
    * fix(backfill): date needs to be 2 digit
    
    * parser backoff with max 3 retry
    
    * move parser caller into an object, and remove the extra file
    
    * remove redundent try-catch
    
    * set return type for getParserMetadata

--- curation-migration-datasync/externalCaller/parser.ts
+++ curation-migration-datasync/externalCaller/parser.ts
@@ -1,5 +1,6 @@
-import fetch from 'node-fetch';
+import { backOff } from 'exponential-backoff';
 import config from '../config';
+import fetch from 'node-fetch';
 
 type ParserMetadataResponse = {
   resolvedId: string;
@@ -12,19 +13,38 @@ type ParserMetadataResponse = {
  * @param url the URL to fetch data for
  * @returns ParserMetadataResponse
  */
+export const parser = {
+  parserCaller: async (url: string): Promise<ParserMetadataResponse> => {
+    const params = new URLSearchParams({
+      output: 'regular',
+      getItem: '1',
+      images: '0',
+      url: url,
+    });
+    const res = await fetch(config.parserEndpoint + '/' + params.toString());
+    const jsonRes = await res.json();
+    return {
+      domainId: jsonRes['item']['domain_id'],
+      resolvedId: jsonRes['resolved_id'],
+    };
+  },
+};
+
+/**
+ * calls parser to fetch domainID and resolvedId.
+ * Retries for max 3 times with backoff.
+ * https://www.npmjs.com/package/exponential-backoff#ibackoffoptions
+ * @param url
+ */
 export async function getParserMetadata(
   url: string
 ): Promise<ParserMetadataResponse> {
-  const params = new URLSearchParams({
-    output: 'regular',
-    getItem: '1',
-    images: '0',
-    url: url,
-  });
-  const res = await fetch(config.parserEndpoint + '/' + params.toString());
-  const jsonRes = await res.json();
-  return {
-    domainId: jsonRes['item']['domain_id'],
-    resolvedId: jsonRes['resolved_id'],
+  const backOffOptions = {
+    numOfAttempts: 3, //default is 10
   };
+
+  return (await backOff(
+    () => parser.parserCaller(url),
+    backOffOptions
+  )) as ParserMetadataResponse;
 }
