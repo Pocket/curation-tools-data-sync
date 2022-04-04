f044b8d8ec19b6c2a1c74de1150a85c99949a7f6
Kat Schelonka <kschelonka@mozilla.com>
2022-04-04 14:11:04 -0700

    deps: install node-fetch

--- curation-migration-datasync/index.ts
+++ curation-migration-datasync/index.ts
@@ -1,11 +1,11 @@
 import config from './config';
 import * as Sentry from '@sentry/serverless';
 import { writeClient } from './database/dbClient';
-import { SQSEvent } from 'aws-lambda';
+import { EventBridgeEvent } from 'aws-lambda';
 import { addScheduledItem } from './eventConsumer';
 import { EventDetailType } from './types';
 
-export async function handlerFn(event: SQSEvent) {
+export async function handlerFn(event: EventBridgeEvent<any, any>) {
   console.log(JSON.stringify(event));
 
   // Check if the feed is included in the allowlist
