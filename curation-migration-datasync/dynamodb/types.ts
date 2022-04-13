2781cddb4301a7bd17bd787dfe55ff819d5b0130
Kat Schelonka <kschelonka@mozilla.com>
2022-04-13 12:54:13 -0700

    fix: add sandbox to feed id mappings

--- curation-migration-datasync/dynamodb/types.ts
+++ curation-migration-datasync/dynamodb/types.ts
@@ -5,6 +5,7 @@ export enum ScheduledSurfaceGuid {
   NEW_TAB_EN_GB = 'NEW_TAB_EN_GB',
   NEW_TAB_EN_INTL = 'NEW_TAB_EN_INTL',
   NEW_TAB_DE_DE = 'NEW_TAB_DE_DE',
+  SANDBOX = 'SANDBOX',
 }
 
 export type CuratedItemRecord = {
