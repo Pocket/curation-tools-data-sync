ad02d1b1fd0c1c6da29291d68a2ef196b5ac40cf
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2022-06-17 07:13:38 +1000

    feat(authors backfill): [BACK-1463] Update lambda to call update mutation (#239)
    * feat(authors backfill): [BACK-1463] Update lambda to call update mutation
    
    * add logging before and after the mutation call
    
    * update logs to capture separate request failures if any
    
    * tweak logs and error throwing
    
    Co-authored-by: Jonathan Petto <jpetto@users.noreply.github.com>
    Co-authored-by: Herraj Luhano <hluhano@mozilla.com>

--- curation-authors-backfill/index.ts
+++ curation-authors-backfill/index.ts
@@ -1,11 +1,19 @@
 import * as Sentry from '@sentry/serverless';
 import config from './config';
 import { SQSEvent, SQSBatchResponse, SQSBatchItemFailure } from 'aws-lambda';
-import { SqsBackfillMessage } from './types';
+import { ApprovedItemAuthor, SqsBackfillMessage } from './types';
 import { fetchProspectData } from './externalCaller/prospectApiCaller';
-import { parseAuthorsCsv } from './lib';
-//import { sleep } from './lib';
-//import { callUpdateMutation } from './externalCaller/importMutationCaller';
+import { parseAuthorsCsv, sleep } from './lib';
+import { callUpdateMutation } from './externalCaller/importMutationCaller';
+
+// simple function to create a string representation of an array of author names and sort orders
+// returns a string like "Jane Austin (1), Octavia Butler (2)"
+function stringifyAuthors(authors: ApprovedItemAuthor[]): string {
+  return authors.reduce((previous, current: ApprovedItemAuthor) => {
+    return `${previous}, ${current.name} (${current.sortOrder})`;
+  }, '');
+}
+
 /**
  * Lambda handler function. Separated from the Sentry wrapper
  * to make unit-testing easier.
@@ -30,9 +38,17 @@ export async function handlerFn(event: SQSEvent): Promise<SQSBatchResponse> {
       title = message.title;
       publisher = message.publisher;
 
-      const prospectData = await fetchProspectData(url);
+      let prospectData;
 
-      let authors = parseAuthorsCsv(prospectData.authors);
+      try {
+        prospectData = await fetchProspectData(url);
+      } catch (error) {
+        throw new Error(
+          `Failed to fetch prospect data from Parser. Reason: ${error}`
+        );
+      }
+
+      let authors: ApprovedItemAuthor[] = parseAuthorsCsv(prospectData.authors);
 
       // if no valid authors were found, default to the publisher
       if (!authors.length) {
@@ -44,58 +60,45 @@ export async function handlerFn(event: SQSEvent): Promise<SQSBatchResponse> {
         ];
       }
 
-      // Wait a sec... don't barrage the api. We're just backfilling here.
+      // Wait a sec... don't barrage the API. We're just backfilling here.
+      await sleep(1000);
 
-      //await sleep(1000);
+      console.log(
+        `MUTATION INPUT: externalId: ${externalId}, publisher: ${publisher}, authors: ${stringifyAuthors(
+          authors
+        )}`
+      );
 
-      //const mutationResponse = await callUpdateMutation({
-      //  externalId,
-      //  authors,
-      //});
+      try {
+        const { data } = await callUpdateMutation({
+          externalId,
+          authors,
+        });
+        console.log(
+          `MUTATION OUTPUT: externalId: ${
+            data.updateApprovedCorpusItemAuthors.externalId
+          }, url: ${data.updateApprovedCorpusItemAuthors.url}, title: ${
+            data.updateApprovedCorpusItemAuthors.title
+          } authors: ${stringifyAuthors(
+            data.updateApprovedCorpusItemAuthors.authors
+          )}`
+        );
+      } catch (error) {
+        throw new Error(
+          `Update mutation call failed for item. Reason: ${error}`
+        );
+      }
 
-      // TODO: do something with the response...
-
-      // copy / pasta code below - keeping for reference for now
-
-      //as json stringify could throw error in catch, which can cause entire batch failure
-      // fetching this value in try, and using them in catch.
-      // curatedRecId = message.curated_rec_id;
-      // resolvedUrl = message.resolved_url;
-      // resolvedId = message.resolved_id;
-      // imageUrl = message.image_src;
-      // const prospectData = await fetchProspectData(message.resolved_url);
-      // const corpusInput = hydrateCorpusInput(message, prospectData);
-      // // Wait a sec... don't barrage the api. We're just backfilling here.
-      // await sleep(1000);
-      // const importMutationResponse = await callImportMutation(corpusInput);
-      // const curatedItemRecord: CuratedItemRecord = {
-      //   curatedRecId: parseInt(message.curated_rec_id),
-      //   scheduledItemExternalId:
-      //     importMutationResponse?.data?.importApprovedCorpusItem.scheduledItem
-      //       .externalId,
-      //   approvedItemExternalId:
-      //     importMutationResponse?.data?.importApprovedCorpusItem.approvedItem
-      //       .externalId,
-      //   scheduledSurfaceGuid:
-      //     ScheduledSurfaceGuid[
-      //       importMutationResponse?.data?.importApprovedCorpusItem.scheduledItem
-      //         .scheduledSurfaceGuid
-      //     ],
-      //   lastUpdatedAt: new Date().getTime(),
-      // };
-      // console.log(`curatedItemRecord -> ${JSON.stringify(curatedItemRecord)}`);
-      //
-      // await insertCuratedItem(dbClient, curatedItemRecord);
+      // Run the `updateApprovedCorpusItemAuthors` mutation
     } catch (error) {
-      console.log(`unable to process message -> externalId: ${externalId},
-       url : ${url}, title: ${title}, publisher: ${publisher}`);
-      console.log(error);
+      console.warn(`unable to process message -> externalId: ${externalId},
+       url : ${url}, title: ${title}, publisher: ${publisher}. Error: ${error}`);
 
       Sentry.captureException(error);
 
       Sentry.addBreadcrumb({
         message: `unable to process message -> externalId: ${externalId},
-       url : ${url}, title: ${title}, publisher: ${publisher}`,
+       url : ${url}, title: ${title}, publisher: ${publisher}. Error: ${error}`,
       });
 
       batchFailures.push({ itemIdentifier: record.messageId });
