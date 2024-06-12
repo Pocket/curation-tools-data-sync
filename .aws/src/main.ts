8a921fd68a14c33485ed95edaa08696c481d4249
Cassandra <145622947+cass-moz@users.noreply.github.com>
2024-06-12 10:22:38 -0700

    fix(tf): Move terraform state to S3.

--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -2,7 +2,7 @@ import { Construct } from 'constructs';
 import {
   App,
   DataTerraformRemoteState,
-  RemoteBackend,
+  S3Backend,
   TerraformStack,
 } from 'cdktf';
 import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
@@ -28,10 +28,11 @@ class CurationToolsDataSync extends TerraformStack {
     new NullProvider(this, 'null_provider');
     new ArchiveProvider(this, 'archive_provider');
 
-    new RemoteBackend(this, {
-      hostname: 'app.terraform.io',
-      organization: 'Pocket',
-      workspaces: [{ prefix: `${config.name}-` }],
+    new S3Backend(this, {
+      bucket: `mozilla-content-team-${config.environment.toLowerCase()}-terraform-state`,
+      dynamodbTable: `mozilla-content-team-${config.environment.toLowerCase()}-terraform-state`,
+      key: config.name,
+      region: 'us-east-1',
     });
 
     // ** shared infrastructure between backfill and datasync
